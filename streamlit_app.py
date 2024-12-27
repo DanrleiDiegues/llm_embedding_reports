6053b5dbed6cd539f1de0fe2a36b2d788d30615c
Danrlei Diegues <danrlei.diegueso@gmail.com>
2024-12-26 23:28:50 -0300

    commit update 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
-try:
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-except ImportError:
-    pass
+import sys
+import pysqlite3
+sys.modules["sqlite3"] = pysqlite3
 
 import streamlit as st
 import pandas as pd
